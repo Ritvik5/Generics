dc6387d7b6d9675ab8e99a011f0dd23f049dc327
Ritvik5 <sharma.ritvik56@gmail.com>
2023-03-09 16:12:54 +0530

    Finding Integer Max Using compareTo

---
+++ Generics/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Generics
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            MaximumCheck max = new MaximumCheck();
+            int result = max.MaximumInteger(4, 2, 3);
+            Console.WriteLine(result);
+
+            result = max.MaximumInteger(2, 4, 3);
+            Console.WriteLine(result);
+
+            result = max.MaximumInteger(2, 3, 4);
+            Console.WriteLine(result);
+        }
+    }
+}
