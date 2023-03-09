4751defc9203b58d500bc0cb705a856aad8a211c
Ritvik5 <sharma.ritvik56@gmail.com>
2023-03-09 16:25:06 +0530

    Finding Float Max using compareTo

--- Generics/Program.cs
+++ Generics/Program.cs
@@ -4,15 +4,25 @@
     {
         public static void Main(string[] args)
         {
-            MaximumCheck max = new MaximumCheck();
-            int result = max.MaximumInteger(4, 2, 3);
-            Console.WriteLine(result);
+            //MaximumCheck imax = new MaximumCheck();
+            //int iresult = imax.MaximumInteger(4, 2, 3);
+            //Console.WriteLine(iresult);
 
-            result = max.MaximumInteger(2, 4, 3);
-            Console.WriteLine(result);
+            //iresult = imax.MaximumInteger(2, 4, 3);
+            //Console.WriteLine(iresult);
 
-            result = max.MaximumInteger(2, 3, 4);
-            Console.WriteLine(result);
+            //iresult = imax.MaximumInteger(2, 3, 4);
+            //Console.WriteLine(iresult);
+
+            MaximumCheck fmax = new MaximumCheck();
+            float fresult = fmax.MaximumFloat(4.2f, 2.2f, 2.3f);
+            Console.WriteLine(fresult);
+
+            fresult = fmax.MaximumFloat(2.4f, 4.6f, 3.4f);
+            Console.WriteLine(fresult);
+
+            fresult = fmax.MaximumFloat(2.4f, 4.6f, 6.9f);
+            Console.WriteLine(fresult);
         }
     }
 }
