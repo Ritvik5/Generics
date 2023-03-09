b7af2d7495d6dcc194149a2041912fe4bd7c5877
Ritvik5 <sharma.ritvik56@gmail.com>
2023-03-09 18:35:20 +0530

    Using Generics for Type safety

---
+++ Generics/GenericsMaximum.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    public class GenericsMaximum<Type> where Type : IComparable
+    {
+        public Type[] value;
+
+        public GenericsMaximum(Type[] value)
+        {
+            this.value = value;
+        }
+
+        public Type[] Sort(Type[] value)
+        {
+            Array.Sort(value);
+            return value;
+        }
+
+        public Type Maximum(Type[] value)
+        {
+            var sortedValues = Sort(value);
+            return sortedValues[sortedValues.Length - 1];
+        }
+
+        public void PrintMaximum()
+        {
+            var max = Maximum(value);
+            Console.WriteLine("Maximum value is : " +max);
+        }
+    }
+}
--- Generics/Program.cs
+++ Generics/Program.cs
@@ -24,9 +24,19 @@
             //fResult = fMax.MaximumFloat(2.4f, 4.6f, 6.9f);
             //Console.WriteLine(fResult);
 
-            MaximumCheck sMax = new MaximumCheck();
-            string sResult = sMax.MaximumString("Apple", "Peach", "Banana");
-            Console.WriteLine(sResult);
+            //MaximumCheck sMax = new MaximumCheck();
+            //string sResult = sMax.MaximumString("Apple", "Peach", "Banana");
+            //Console.WriteLine(sResult);
+
+            int[] arrayOne = { 1, 111, 1255, 132, 14, 155, 167 };
+            GenericsMaximum<int> intGenerics = new GenericsMaximum<int>(arrayOne);
+            intGenerics.PrintMaximum();
+            float[] arrayTwo = { 20.1f, 12.5f, 203.4f, 15.5f, 60.5f };
+            GenericsMaximum<float> floatGenerics = new GenericsMaximum<float>(arrayTwo); 
+            floatGenerics.PrintMaximum();
+            string[] arrayThree = { "Tom", "John", "Smith", "Will", "Jack" };
+            GenericsMaximum<string> stringGenerics = new GenericsMaximum<string>(arrayThree);
+            stringGenerics.PrintMaximum();
         }
     }
 }
